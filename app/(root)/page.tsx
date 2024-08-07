d43f6832001b3067bff33051b3d70c6e6e22ee13
Grigorios Alexandrou <t8220004@aueb.gr>
2024-08-08 02:13:35 +0300

    right sidebar cards semi complete

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = {firstName: 'Gregory'};
+  const loggedIn = {firstName: 'Gregory', lastName: 'Alexandrou', email:'alexandrougrigorios@gmail.com'};
 
 
   return (
@@ -24,7 +25,17 @@ const Home = () => {
               totalCurrentBalance={1250.35}
           />
         </header>
+
+
+        RECENT TRANSACTIONS
       </div>
+
+
+      <RightSidebar 
+          user={loggedIn}
+          transactions={[]}
+          banks={[{currentBalance: 123.50}, {currentBalance: 500.50}]}
+      />
     </section>
   )
 }
