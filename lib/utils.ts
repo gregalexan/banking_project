55aa47f5567ae5563219fe4e438a307f5fa245df
Grigorios Alexandrou <t8220004@aueb.gr>
2024-08-08 19:54:58 +0300

    sign in sign up pages

--- lib/utils.ts
+++ lib/utils.ts
@@ -193,3 +193,18 @@ export const getTransactionStatus = (date: Date) => {
 
   return date > twoDaysAgo ? "Processing" : "Success";
 };
+
+import {z} from 'zod'
+export const authFormSchema = (type: string) => z.object({
+  // Sign Up
+  firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+  state: type === 'sign-in' ? z.string().optional() : z.string().max(2).min(2),
+  postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
+  dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  // Both
+  email: z.string().email(),
+  password: z.string().min(8),
+})
