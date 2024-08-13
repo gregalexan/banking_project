cd9c5586bc68466fe1083c9a20d087d442d07501
Grigorios Alexandrou <t8220004@aueb.gr>
2024-08-13 23:59:59 +0300

    Finally done everything, will come back for upgrades

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/HeaderBox'
+import { Pagination } from '@/components/Pagination';
 import TransactionsTable from '@/components/TransactionsTable';
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
@@ -12,7 +13,12 @@ const TransactionHistory = async ({searchParams: {id, page}}: SearchParamProps) 
   if (!accounts) return;
   const accountsData = accounts?.data
   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
-  const account = await getAccount({appwriteItemId})
+  const account = await getAccount({appwriteItemId});
+  const rowsPerPage = 10;
+  const totalPages = Math.ceil(account?.transactions.length / rowsPerPage);
+  const indexOfLastTransaction = currentPage * rowsPerPage;
+  const indexOfFirstTransaction = indexOfLastTransaction - rowsPerPage;
+  const currentTransactions = account?.transactions.slice(indexOfFirstTransaction, indexOfLastTransaction);
   return (
     <section className='transactions'>
       <div className='transactions-header'>
@@ -45,8 +51,13 @@ const TransactionHistory = async ({searchParams: {id, page}}: SearchParamProps) 
           </div>
           <section className='flex w-full flex-col gap-6'>
               <TransactionsTable 
-                  transactions={account?.transactions}
+                  transactions={currentTransactions}
               />
+              {totalPages > 1 && (
+                        <div className='my-4 w-full'>
+                            <Pagination totalPages={totalPages} page={currentPage} />
+                        </div>
+                    )}
           </section>
       </div>
     </section>
